c55369428fa06228b92542686127dc25fc17d09e
Ipatiov Grigor <grigor.ipatiov@algotech.solutions>
2019-07-25 12:11:00 +0300

    no message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "./Button.js";
-import Display from "./Display.js";
-import constant from "./constants";
-import "./Style.css";
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button';
+import Display from './Display';
+import { VALUES_ARRAY, NUMBERS } from './constants';
+import './Style.css';
 
-const App = () => {
-  let index = 0;
-  let arr = constant.arr;
-  return (
-    <>
-      <Display />
-      <div className="ButtonsArea">
-        {arr.map(value => (
-          <Button key={value} index={index++} value={value} />
-        ))}
-      </div>
-    </>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: '0',
+      default: true
+    };
+    //this.updateResult = this.updateResult.bind(this);
+  }
+  valuesArray = VALUES_ARRAY;
+  updateResult = (value, defaultValue) => {
+    if (defaultValue === true) {
+      this.setState({ result: '0', default: defaultValue });
+    }
+    else
+      if (this.state.default !== true)
+        this.setState({
+          result: this.state.result.concat(value),
+          default: defaultValue
+        });
+        else {
+          this.setState({ result: value, default: defaultValue });
+        }
+  };
+  isDisplayable(index) {
+    if (NUMBERS.indexOf(index) === -1) return false;
+    return true;
+  }
+  render() {
+    return (
+      <>
+        <Display value={this.state.result} />
+        <div className='buttons-area'>
+          {this.valuesArray.map((value,index) => (
+            <Button
+              key={value}
+              isDisplayable={this.isDisplayable(index)}
+              index={index++}
+              value={
+                value === 'AC' && this.state.default === false ? 'C' : value
+              }
+              updateResult={this.updateResult}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,18 +1,38 @@
-import React from "react";
-import "./Style.css";
-import constant from "./constants";
+import React from 'react';
+import './Style.css';
+import {COLORS} from './constants';
 
-let colors = constant.colors;
-const Button = props => (
-  <button
-    className="Button"
-    style={{
-      width: props.value === "0" ? "120px" : "60px",
-      backgroundColor: colors[props.index]
-    }}
-  >
-    {props.value}
-  </button>
-);
+let colors = COLORS;
+class Button extends React.Component {
+  specialKeys = () =>{
+    switch(this.props.value){
+      case 'C':
+        this.props.updateResult('0',true);
+        break;
+      default:
+        console.log('this is a special key', this.props.value);
+    }
+  };
+
+  updateResult = () => {
+    if (this.props.isDisplayable)
+      this.props.updateResult(this.props.value, false);
+    else this.specialKeys();
+  };
+  render() {
+    return (
+      <button
+        className='button'
+        style={{
+          backgroundColor: colors[this.props.index],
+          width: this.props.value === '0' ? '120px' : '60px'
+        }}
+        onClick={this.updateResult}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
 
 export default Button;
